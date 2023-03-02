0a0aa623d231f55fb2a89785b61bfcf7ae34ecb3
Raheem Lawal <lawalraheem@gmail.com>
2023-03-02 14:34:23 -0800

    changed test in ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn how to do a pick and roll!
         </a>
       </header>
     </div>
